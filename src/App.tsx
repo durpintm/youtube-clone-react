5e3353b57ca98f63df3681dcb1890da7bb7bced9
Durpin Thapa Magar <durpinthapa@gmail.com>
2024-05-02 17:01:12 -0400

    Initializing react youtube clone project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      {/* <img
+        className="h-8 mx-2"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Logo_of_YouTube_%282013-2015%29.svg/2560px-Logo_of_YouTube_%282013-2015%29.svg.png"
+        alt="Youtube Logo"
+      /> */}
+      <h1>Youtube React</h1>
+    </>
+  );
+}
+
+export default App;
