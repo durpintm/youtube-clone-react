5ff68c761fc3001d2be084e8ac4fb87d977f0fd9
Durpin Thapa Magar <durpinthapa@gmail.com>
2024-05-03 00:01:30 -0400

    Category and Page Header section added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import PageHeader from "./layout/PageHeader";
+import { CategoryPills } from "./components/CategoryPills";
+import { categories } from "./data/home";
+import { useState } from "react";
 
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState(categories[0]);
   return (
     <div className="max-h-screen flex flex-col">
       <PageHeader />
-      <div>1</div>
+      <div className="grid grid-cols-[auto, 1fr] flex flex-grow-1 overflow-auto">
+        <div>Sidebar</div>
+        <div className="overflow-x-hidden px-8 pb-4">
+          <div className="sticky top-0 bg-white z-10 pb-4">
+            <CategoryPills
+              categories={categories}
+              selectedCategory={selectedCategory}
+              onSelect={setSelectedCategory}
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
